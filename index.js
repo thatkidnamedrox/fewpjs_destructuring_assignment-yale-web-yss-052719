4d4d7f6aea97601d98d403649ff62bda345bf04c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 02:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,3 +54,4 @@ const {name, color, song, job, partner} = muppet
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song_2, song_4} = k_muppet.k_album.muppet_movie
